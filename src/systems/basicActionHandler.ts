a9d25b875aa743a3000f0a348bf742539f38a9f2
SimonKindhauser <simon.kindhauser@bitmessage.ch>
2018-03-03 11:59:37 +0100

    adds menu actions to basics handler

--- src/systems/basicActionHandler.ts
+++ src/systems/basicActionHandler.ts
@@ -11,6 +11,21 @@ namespace sczBase
     stop(requires: Array<sczCore.Component>): void;
     quickSave(requires: Array<sczCore.Component>): void;
     quickLoad(requires: Array<sczCore.Component>): void;
+
+    // menu navigation
+    up(requires: Array<sczCore.Component>): void;
+    down(requires: Array<sczCore.Component>): void;
+    left(requires: Array<sczCore.Component>): void;
+    right(requires: Array<sczCore.Component>): void;
+    previous(requires: Array<sczCore.Component>): void;
+    next(requires: Array<sczCore.Component>): void;
+    back(requires: Array<sczCore.Component>): void;
+
+    // menu actions
+    select(requires: Array<sczCore.Component>): void;
+    unselect(requires: Array<sczCore.Component>): void;
+    undo(requires: Array<sczCore.Component>): void;
+    redo(requires: Array<sczCore.Component>): void;
 }
 
   export class BasicActionHandlerBase implements BasicActionHandler
@@ -22,5 +37,20 @@ namespace sczBase
     stop(_: sczCore.Component[]): void {}
     quickSave(_: sczCore.Component[]): void {}
     quickLoad(_: sczCore.Component[]): void {}
+
+    // menu navigation
+    up(_: sczCore.Component[]): void {}
+    down(_: sczCore.Component[]): void {}
+    left(_: sczCore.Component[]): void {}
+    right(_: sczCore.Component[]): void {}
+    previous(_: sczCore.Component[]): void {}
+    next(_: sczCore.Component[]): void {}
+    back(_: sczCore.Component[]): void {}
+
+    // menu actions
+    select(_: Array<sczCore.Component>): void {}
+    unselect(_: Array<sczCore.Component>): void {}
+    undo(_: sczCore.Component[]): void {}
+    redo(_: sczCore.Component[]): void {}
   }
 }
