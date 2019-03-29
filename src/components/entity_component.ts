0931c371e759034f05031499bb68b13dc5fe88c1
Simon Kindhauser <simon.kindhauser@bitmessage.ch>
2019-03-29 12:23:30 +0100

    adds component with reference to entity

---
+++ src/components/entity_component.ts
@@ -0,0 +1,9 @@
+/// <reference path="../../node_modules/sancasia_zero-core/obj/sancasia_zero.core.d.ts" />
+
+namespace sczBase
+{
+  export class EntityComponent extends sczCore.Component
+  {
+    public reference: sczCore.Entity;
+  }
+}
